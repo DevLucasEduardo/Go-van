1b4a370fa4afefeac5791e87721773f70feda451
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-09-27 21:35:34 -0300

    feat: header component

---
+++ src/app/components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="bg-blue-500 flex text-white justify-between items-center">
+      <h1 className="margin m-8 text-5xl font-bold">VanGo</h1>
+      <nav></nav>
+    </header>
+  );
+}
