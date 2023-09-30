09f24fceb0e22de405f2ce63decc95f7dce78f93
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-09-30 16:05:34 -0300

    feat: new page for registered journeys

---
+++ src/app/sign/page.tsx
@@ -0,0 +1,9 @@
+import SearchBar from "../components/search-bar";
+
+export default function SignIn() {
+  return (
+    <div>
+      <SearchBar placeholder="Pesquisar por destino"></SearchBar>
+    </div>
+  );
+}
