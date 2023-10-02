dfedca82430330f0afab0868dd876764eae52e03
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-10-01 21:34:23 -0300

    refactor: header added Link tags for routing

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="bg-gray-800 flex text-white justify-between items-center">
-      <h1 className="margin m-8 text-5xl font-bold">VanGo</h1>
+      <Link href="/" className="m-8 text-5xl font-bold">
+        VanGo
+      </Link>
+      <nav className="m-8">
+        <ul>
+          <Link href="/sign">Login</Link>
+        </ul>
+      </nav>
     </header>
   );
 }
