fc2272fa9df5b771104cb103ff238578b99de6f8
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-10-01 21:35:08 -0300

    feat: search bar filters cards

--- src/app/components/search-bar.tsx
+++ src/app/components/search-bar.tsx
@@ -1,34 +1,51 @@
-"use client";
-
 import React from "react";
 
 interface Search {
   placeholder: string;
+  sendSearchBarInfo: (data: string) => void;
 }
+
 const SearchBar = (props: Search) => {
+  const [data, setData] = React.useState("");
+
+  const handleChange = ({ target }: any) => {
+    setData(target.value);
+  };
+
+  const handleClick = () => {
+    props.sendSearchBarInfo(data);
+  };
+
   return (
     <div className="flex justify-center mt-16">
-      <div className="relative w-80 flex">
+      <div className="w-80 relative">
         <input
           type="text"
           placeholder={props.placeholder}
-          className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          className="placeholder-italic placeholder-text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-10 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          onChange={handleChange}
         />
-        <svg
-          width="24"
-          height="24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          className=" text-slate-300 dark:text-slate-400 absolute m-1.5"
-          aria-hidden="true"
+        <button
+          className="bg-zinc-50 absolute top-0.5 right-1 h-8.5 p-1 bg-inherit"
+          onClick={handleClick}
         >
-          <path d="m19 19-3.5-3.5"></path>
-          <circle cx="11" cy="11" r="6"></circle>
-        </svg>
+          <svg
+            width="24"
+            height="24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="text-slate-300 dark:text-slate-400"
+            aria-hidden="true"
+          >
+            <path d="M19 19L15.5 15.5"></path>
+            <circle cx="11" cy="11" r="6"></circle>
+          </svg>
+        </button>
       </div>
+      {}
     </div>
   );
 };
