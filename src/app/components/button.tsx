cce37f75a36a3b63abd345620ea6fdbcd9616626
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-10-01 21:32:55 -0300

    refactor: Style change and addition of Link tag

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import Link from "next/link";
 
 interface Info {
   label: string;
+  href: string;
 }
 
 const Button = (props: Info) => {
   return (
-    <button className="bg-blue-500 rounded-sm text-white	p-1.5 m-1.5">
+    <Link
+      className="bg-gradient-to-r from-gray-800 to-sky-900 ... rounded-md p-6 text-white"
+      href={props.href}
+    >
       {props.label}
-    </button>
+    </Link>
   );
 };
 
