40eaf5edafa4a1f57a89d8fe8999e599ba0fcc69
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-10-01 21:31:53 -0300

    refactor: header added to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "./components/header";
-
+import Footer from "./components/footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
