800a838c8603f4894403ff102420e7836d1b52f1
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-10-09 00:40:08 -0300

    feat: slogan section and css changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,47 @@
-import Button from "./components/button";
-import Section from "./components/section";
-
+import Button from "./components/link";
+import Image from "next/image";
+import van from "./public/images/van.jpg";
 export default function Home() {
   return (
     <>
-      <Section content="VanGo é uma inovadora plataforma de transporte que busca solucionar os desafios enfrentados ao buscar meios de locomoção confiáveis, eficientes e acessíveis. Nosso objetivo principal é reduzir o tráfego nas estradas, mitigar os impactos ambientais relacionados à emissão de poluentes e proporcionar segurança tanto para os prestadores de serviços quanto para os passageiros." />
+      <div className="max-w-5xl mx-auto">
+        <section className="flex justify-center font-bold text-7xl">
+          <h1 className="w-5/6 text-center my-20">
+            Mobilidade Inteligente com VanGo
+          </h1>
+        </section>
 
-      <section className="flex justify-center flex-wrap max-w-4xl mx-auto pt-28">
-        <Button label="Visualizar transportes disponíveis" href="/find" />
-      </section>
-
-      <Section
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non ullam
-          officia modi possimus maiores, ipsam obcaecati laboriosam rerum
-          veniam. Optio officia ipsa magnam tempora libero voluptate cumque
-          similique cum maxime.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo culpa neque ipsa ab voluptatem! Voluptatum tempora in suscipit ea tenetur repudiandae sint blanditiis ullam. Totam assumenda temporibus praesentium ad quidem."
-      />
+        <section className="flex justify-center flex-wrap mb-40">
+          <Button
+            label="Visualizar transportes disponíveis"
+            href="/find"
+            px="px-40"
+          />
+        </section>
+        <hr></hr>
+        <section className=" mt-20 gap-5 flex">
+          <div className="flex-col basis-9/12">
+            <h1 className="font-bold text-5xl mb-6">Sobre nós</h1>
+            <p className="text-xl text-gray-500">
+              VanGo é uma inovadora plataforma de transporte que busca
+              solucionar os desafios enfrentados ao buscar meios de locomoção
+              confiáveis, eficientes e acessíveis. Nosso objetivo principal é
+              reduzir o tráfego nas estradas, mitigar os impactos ambientais
+              relacionados à emissão de poluentes e proporcionar segurança tanto
+              para os prestadores de serviços quanto para os passageiros.
+            </p>
+          </div>
+          <div className="">
+            <Image
+              src={van}
+              width={500}
+              height={500}
+              alt="Picture of the author"
+              className="rounded-md"
+            />
+          </div>
+        </section>
+      </div>
     </>
   );
 }
