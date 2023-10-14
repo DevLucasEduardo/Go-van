747afdffa79c5ea482d068a53d19f3d2b1b11163
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-10-14 17:42:53 -0300

    feat: addition of the form for login

--- src/app/sign/page.tsx
+++ src/app/sign/page.tsx
@@ -1,9 +1,13 @@
-import SearchBar from "../components/search-bar";
-
-export default function SignIn() {
+import Form from "../components/form";
+import Button from "../components/link";
+export default function Find() {
   return (
-    <div>
-      <SearchBar placeholder="Pesquisar por destino"></SearchBar>
+    <div className="flex justify-center">
+      <div className="flex flex-col items-center mt-20 border rounded-xl max-w-3xl px-20 py-10 gap-4">
+        <Form type="email" id="1" label="E-mail" />
+        <Form type="password" id="2" label="Senha" />
+        <Button label="Continuar" href="/register" px="px-10" />
+      </div>
     </div>
   );
 }
