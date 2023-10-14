88d3874dc1ae0b95d6e22de5c2fa8eaebfab537c
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-10-14 17:42:32 -0300

    feat: addition of  the form component

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,3 +1,15 @@
+import Form from "../components/form";
+
 export default function Register() {
-  return <></>;
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col items-center mt-20 border rounded-sm max-w-3xl px-20 py-10 gap-4">
+        <Form type="text" id="1" label="Veículo" />
+        <Form type="text" id="1" label="Destino" />
+        <Form type="text" id="1" label="Lugares disponíveis" />
+        <Form type="date" id="1" label="Data de partida" />
+        <Form type="time" id="1" label="Data de partida" />
+      </div>
+    </div>
+  );
 }
