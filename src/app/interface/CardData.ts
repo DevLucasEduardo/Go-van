4ae494744fa35d9d638f68ed3044a043a0ac9951
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-11-13 02:12:43 -0300

    refactor: interface addapted

---
+++ src/app/interface/CardData.ts
@@ -0,0 +1,10 @@
+export default interface CardData {
+  vehicle: string;
+  licensePlate: string;
+  departureLocation: string;
+  destination: string;
+  availableSeats: number;
+  departureDate: string;
+  departureTime: string;
+  accessibility: boolean;
+}
