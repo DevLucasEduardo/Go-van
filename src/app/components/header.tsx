37a9bd0c57b0d6940fb8e525364ebd73d255016d
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-11-13 02:07:25 -0300

    feat: minor changes

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,11 +5,11 @@ export default function Header() {
     <header className="">
       <div className="max-w-6xl mx-auto flex  justify-between items-center">
         <Link href="/" className="my-2 text-5xl font-bold">
-          VanGo
+          GoVan
         </Link>
         <nav>
           <ul>
-            <Button label="Login" href="/sign" px="px-10" />
+            <Button label="Login" href="/page-login" px="px-10" />
           </ul>
         </nav>
       </div>
