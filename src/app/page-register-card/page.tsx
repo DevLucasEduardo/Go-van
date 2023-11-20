c29d7aa06ebaf75c0f2d35e347aacc2c86838f34
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-11-20 01:38:19 -0300

    feat: forms validation and toastify alert

--- src/app/page-register-card/page.tsx
+++ src/app/page-register-card/page.tsx
@@ -4,8 +4,13 @@ import Form from "../components/form";
 import { useState } from "react";
 
 import axios from "axios";
+import Header from "../components/header";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Link from "next/link";
 
 export default function Register() {
+  const [isEmpty, setIsEmpty] = useState(false);
   const [formData, setFormData] = useState({
     vehicle: "",
     licensePlate: "",
@@ -17,7 +22,21 @@ export default function Register() {
     accessibility: false,
   });
 
+  const notify = () => {
+    toast.success("Viagem registrada!", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
+
   const handleChange = (event: any) => {
+    setIsEmpty(false);
     let { name, value } = event.target;
 
     if (name === "accessibility") {
@@ -27,16 +46,29 @@ export default function Register() {
         value = false;
       }
     }
-
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value,
     }));
   };
 
-  const handleClick = () => {
-    const result = postData(formData, "cards/post");
-    window.alert("Viagem registrada");
+  const handleClick = (event: any) => {
+    let allValuesNotEmpty = Object.values(formData).every(
+      (value) => value !== ""
+    );
+    const { name, value } = event.target;
+
+    if (name === "accessibility" && value === "") {
+      allValuesNotEmpty = false;
+    }
+
+    if (!allValuesNotEmpty) {
+      setIsEmpty(true);
+      return;
+    }
+    const result = postData(formData, "cards/");
+    notify();
+    clearFields();
   };
 
   const postData = async (data: {}, endpoint: string) => {
@@ -48,16 +80,32 @@ export default function Register() {
     }
   };
 
+  const clearFields = () => {
+    setFormData({
+      vehicle: "",
+      licensePlate: "",
+      departureLocation: "",
+      destination: "",
+      availableSeats: "",
+      departureDate: "",
+      departureTime: "",
+      accessibility: false,
+    });
+  };
+
   return (
     <>
+      <Header logOut={true} />
+
       <h1 className="text-center bold text-3xl">Registrar viagem</h1>
       <div className="max-w-3xl mx-auto">
-        <form className="gap-4 flex flex-wrap justify-between pb-10 pt-10">
+        <form className="gap-4 flex flex-wrap justify-between py-10">
           <Form
             type="text"
             id="1"
             name="vehicle"
             label="Veículo"
+            value={formData.vehicle}
             onChange={handleChange}
           />
           <Form
@@ -65,6 +113,7 @@ export default function Register() {
             id="2"
             name="licensePlate"
             label="Placa"
+            value={formData.licensePlate}
             onChange={handleChange}
           />
           <Form
@@ -72,6 +121,7 @@ export default function Register() {
             id="3"
             name="departureLocation"
             label="Local de partida"
+            value={formData.departureLocation}
             onChange={handleChange}
           />
           <Form
@@ -79,6 +129,7 @@ export default function Register() {
             id="4"
             name="destination"
             label="Destino"
+            value={formData.destination}
             onChange={handleChange}
           />
           <Form
@@ -86,6 +137,7 @@ export default function Register() {
             id="6"
             name="departureDate"
             label="Data de partida"
+            value={formData.departureDate}
             onChange={handleChange}
           />
           <Form
@@ -93,6 +145,7 @@ export default function Register() {
             id="7"
             name="departureTime"
             label="Hora de partida"
+            value={formData.departureTime}
             onChange={handleChange}
           />
           <Form
@@ -100,6 +153,7 @@ export default function Register() {
             id="5"
             name="availableSeats"
             label="Assentos disponíveis"
+            value={formData.availableSeats}
             onChange={handleChange}
           />
 
@@ -117,6 +171,12 @@ export default function Register() {
             </select>
           </div>
         </form>
+
+        {isEmpty && (
+          <div className="flex justify-center pb-5">
+            <p className="text-red-600">Há campos não preenchidos.</p>
+          </div>
+        )}
         <div className="flex justify-center mb-10 self-end">
           <button
             onClick={handleClick}
@@ -124,6 +184,18 @@ export default function Register() {
           >
             Registrar
           </button>
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
         </div>
       </div>
     </>
