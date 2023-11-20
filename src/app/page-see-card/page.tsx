15e253dd12ba73f0ab377fcc6edcc92c82baba23
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-11-20 01:38:39 -0300

    feat: go back button

--- src/app/page-see-card/page.tsx
+++ src/app/page-see-card/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import CardData from "../interface/CardData";
+import Header from "../components/header";
+import Button from "../components/button";
 
 export default function SeeCard({
   searchParams,
@@ -17,6 +19,7 @@ export default function SeeCard({
 }) {
   return (
     <>
+      <Header button={true} />
       <div className="max-w-2xl mx-auto h-64 rounded-2xl bg-slate-200 flex flex-col justify-center">
         <div className="flex flex-col gap-2 text-center">
           <h1 className="font-bold text-sm">{searchParams.vehicle}</h1>
@@ -30,6 +33,9 @@ export default function SeeCard({
             <li>Acessibilidade: {searchParams.accessibility}</li>
           </ul>
         </div>
+      </div>
+      <div className="flex justify-center mt-10">
+        <Button label={"Voltar"} href={"/page-cards"} px={"px-10"} />
       </div>
     </>
   );
